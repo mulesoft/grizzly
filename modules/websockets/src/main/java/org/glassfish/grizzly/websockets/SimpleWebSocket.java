6accee0321b26136070a32709a83566b952d2393
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2018-10-09 15:14:14 -0300

    EE-6323: Introduce WebSockets connector (#9)

--- modules/websockets/src/main/java/org/glassfish/grizzly/websockets/SimpleWebSocket.java
+++ modules/websockets/src/main/java/org/glassfish/grizzly/websockets/SimpleWebSocket.java
@@ -40,297 +40,305 @@
 
 package org.glassfish.grizzly.websockets;
 
+import static org.glassfish.grizzly.utils.Futures.completable;
+import static org.glassfish.grizzly.websockets.Utils.completedFrame;
+
+import java.util.Collection;
+import java.util.EnumSet;
+import java.util.Queue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.glassfish.grizzly.Buffer;
 import org.glassfish.grizzly.Connection;
-import org.glassfish.grizzly.Grizzly;
 import org.glassfish.grizzly.GrizzlyFuture;
 import org.glassfish.grizzly.memory.Buffers;
 import org.glassfish.grizzly.memory.MemoryManager;
 import org.glassfish.grizzly.websockets.frametypes.PingFrameType;
 import org.glassfish.grizzly.websockets.frametypes.PongFrameType;
 
-import java.util.Collection;
-import java.util.EnumSet;
-import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.logging.Logger;
-
 /**
  * Generic WebSocket implementation.  Typically used by client implementations.
  */
 public class SimpleWebSocket implements WebSocket {
 
-    private static final Logger LOGGER = Grizzly.logger(DefaultWebSocket.class);
+  protected final Queue<WebSocketListener> listeners = new ConcurrentLinkedQueue<>();
 
-    protected final Queue<WebSocketListener> listeners =
-            new ConcurrentLinkedQueue<WebSocketListener>();
+  protected final ProtocolHandler protocolHandler;
 
-    protected final ProtocolHandler protocolHandler;
+  protected Broadcaster broadcaster = new DummyBroadcaster();
 
-    protected Broadcaster broadcaster = new DummyBroadcaster();
+  protected enum State {
+    NEW, CONNECTED, CLOSING, CLOSED
+  }
 
-    protected enum State {
-        NEW, CONNECTED, CLOSING, CLOSED
+  protected final EnumSet<State> connected = EnumSet.range(State.CONNECTED, State.CLOSING);
+  protected final AtomicReference<State> state = new AtomicReference<>(State.NEW);
+
+
+  public SimpleWebSocket(final ProtocolHandler protocolHandler,
+                         final WebSocketListener... listeners) {
+
+    this.protocolHandler = protocolHandler;
+    for (WebSocketListener listener : listeners) {
+      add(listener);
     }
 
-    protected final EnumSet<State> connected = EnumSet.range(State.CONNECTED, State.CLOSING);
-    protected final AtomicReference<State> state = new AtomicReference<State>(State.NEW);
+    protocolHandler.setWebSocket(this);
+  }
 
+  public Collection<WebSocketListener> getListeners() {
+    return listeners;
+  }
 
-    public SimpleWebSocket(final ProtocolHandler protocolHandler,
-                            final WebSocketListener... listeners) {
+  @Override
+  public final boolean add(WebSocketListener listener) {
+    return listeners.add(listener);
+  }
 
-        this.protocolHandler = protocolHandler;
-        for (WebSocketListener listener : listeners) {
-            add(listener);
-        }
+  @Override
+  public final boolean remove(WebSocketListener listener) {
+    return listeners.remove(listener);
+  }
 
-        protocolHandler.setWebSocket(this);
+  @Override
+  public boolean isConnected() {
+    return connected.contains(state.get());
+  }
+
+  public void setClosed() {
+    state.set(State.CLOSED);
+  }
+
+  @Override
+  public void onClose(final DataFrame frame) {
+    if (state.compareAndSet(State.CONNECTED, State.CLOSING)) {
+      final ClosingFrame closing = (ClosingFrame) frame;
+      protocolHandler.close(closing.getCode(), closing.getTextPayload());
+    } else {
+      state.set(State.CLOSED);
+      protocolHandler.doClose();
     }
 
-    public Collection<WebSocketListener> getListeners() {
-        return listeners;
+    WebSocketListener listener;
+    while ((listener = listeners.poll()) != null) {
+      listener.onClose(this, frame);
+    }
+  }
+
+  @Override
+  public void onConnect() {
+    state.set(State.CONNECTED);
+
+    for (WebSocketListener listener : listeners) {
+      listener.onConnect(this);
+    }
+  }
+
+  @Override
+  public void onFragment(boolean last, byte[] fragment) {
+    for (WebSocketListener listener : listeners) {
+      listener.onFragment(this, fragment, last);
+    }
+  }
+
+  @Override
+  public void onFragment(boolean last, String fragment) {
+    for (WebSocketListener listener : listeners) {
+      listener.onFragment(this, fragment, last);
+    }
+  }
+
+  @Override
+  public void onMessage(byte[] data) {
+    for (WebSocketListener listener : listeners) {
+      listener.onMessage(this, data);
+    }
+  }
+
+  @Override
+  public void onMessage(String text) {
+    for (WebSocketListener listener : listeners) {
+      listener.onMessage(this, text);
+    }
+  }
+
+  @Override
+  public void onPing(DataFrame frame) {
+    send(new DataFrame(new PongFrameType(), frame.getBytes()));
+    for (WebSocketListener listener : listeners) {
+      listener.onPing(this, frame.getBytes());
+    }
+  }
+
+  @Override
+  public void onPong(DataFrame frame) {
+    for (WebSocketListener listener : listeners) {
+      listener.onPong(this, frame.getBytes());
+    }
+  }
+
+  @Override
+  public void close() {
+    close(NORMAL_CLOSURE, null);
+  }
+
+  @Override
+  public void close(int code) {
+    close(code, null);
+  }
+
+  @Override
+  public void close(int code, String reason) {
+    if (state.compareAndSet(State.CONNECTED, State.CLOSING)) {
+      protocolHandler.close(code, reason);
+    }
+  }
+
+  @Override
+  public CompletableFuture<DataFrame> completableClose(int code, String reason) {
+    if (state.compareAndSet(State.CONNECTED, State.CLOSING)) {
+      return completable(protocolHandler.close(code, reason));
     }
 
-    @Override
-    public final boolean add(WebSocketListener listener) {
-        return listeners.add(listener);
+    return completedFrame(new byte[0], true);
+  }
+
+  @Override
+  public GrizzlyFuture<DataFrame> send(byte[] data) {
+    if (isConnected()) {
+      return protocolHandler.send(data);
+    } else {
+      throw new RuntimeException("Socket is not connected.");
     }
+  }
 
-    @Override
-    public final boolean remove(WebSocketListener listener) {
-        return listeners.remove(listener);
+  @Override
+  public GrizzlyFuture<DataFrame> send(String data) {
+    if (isConnected()) {
+      return protocolHandler.send(data);
+    } else {
+      throw new RuntimeException("Socket is not connected.");
     }
+  }
 
-    @Override
-    public boolean isConnected() {
-        return connected.contains(state.get());
+  @Override
+  public void broadcast(Iterable<? extends WebSocket> recipients,
+                        String data) {
+    if (state.get() == State.CONNECTED) {
+      broadcaster.broadcast(recipients, data);
+    } else {
+      throw new RuntimeException("Socket is already closed.");
     }
+  }
 
-    public void setClosed() {
-        state.set(State.CLOSED);
+  @Override
+  public void broadcast(Iterable<? extends WebSocket> recipients,
+                        byte[] data) {
+    if (state.get() == State.CONNECTED) {
+      broadcaster.broadcast(recipients, data);
+    } else {
+      throw new RuntimeException("Socket is already closed.");
     }
+  }
 
-    @Override
-    public void onClose(final DataFrame frame) {
-        if (state.compareAndSet(State.CONNECTED, State.CLOSING)) {
-            final ClosingFrame closing = (ClosingFrame) frame;
-            protocolHandler.close(closing.getCode(), closing.getTextPayload());
-        } else {
-            state.set(State.CLOSED);
-            protocolHandler.doClose();
-        }
-
-        WebSocketListener listener;
-        while ((listener = listeners.poll()) != null) {
-            listener.onClose(this, frame);
-        }
+  @Override
+  public void broadcastFragment(Iterable<? extends WebSocket> recipients,
+                                String data, boolean last) {
+    if (state.get() == State.CONNECTED) {
+      broadcaster.broadcastFragment(recipients, data, last);
+    } else {
+      throw new RuntimeException("Socket is already closed.");
     }
+  }
 
-    @Override
-    public void onConnect() {
-        state.set(State.CONNECTED);
-
-        for (WebSocketListener listener : listeners) {
-            listener.onConnect(this);
-        }
+  @Override
+  public void broadcastFragment(Iterable<? extends WebSocket> recipients,
+                                byte[] data, boolean last) {
+    if (state.get() == State.CONNECTED) {
+      broadcaster.broadcastFragment(recipients, data, last);
+    } else {
+      throw new RuntimeException("Socket is already closed.");
     }
+  }
 
-    @Override
-    public void onFragment(boolean last, byte[] fragment) {
-        for (WebSocketListener listener : listeners) {
-            listener.onFragment(this, fragment, last);
-        }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public GrizzlyFuture<DataFrame> sendPing(byte[] data) {
+    return send(new DataFrame(new PingFrameType(), data));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public GrizzlyFuture<DataFrame> sendPong(byte[] data) {
+    return send(new DataFrame(new PongFrameType(), data));
+  }
+
+  private GrizzlyFuture<DataFrame> send(DataFrame frame) {
+    if (isConnected()) {
+      return protocolHandler.send(frame);
+    } else {
+      throw new RuntimeException("Socket is not connected.");
     }
+  }
 
-    @Override
-    public void onFragment(boolean last, String fragment) {
-        for (WebSocketListener listener : listeners) {
-            listener.onFragment(this, fragment, last);
-        }
+  @Override
+  public GrizzlyFuture<DataFrame> stream(boolean last, String fragment) {
+    if (isConnected()) {
+      return protocolHandler.stream(last, fragment);
+    } else {
+      throw new RuntimeException("Socket is not connected.");
     }
+  }
 
-    @Override
-    public void onMessage(byte[] data) {
-        for (WebSocketListener listener : listeners) {
-            listener.onMessage(this, data);
-        }
+  @Override
+  public GrizzlyFuture<DataFrame> stream(boolean last, byte[] bytes, int off, int len) {
+    if (isConnected()) {
+      return protocolHandler.stream(last, bytes, off, len);
+    } else {
+      throw new RuntimeException("Socket is not connected.");
     }
+  }
 
-    @Override
-    public void onMessage(String text) {
-        for (WebSocketListener listener : listeners) {
-            listener.onMessage(this, text);
-        }
-    }
+  protected byte[] toRawData(String text) {
+    return toRawData(text, true);
+  }
 
-    @Override
-    public void onPing(DataFrame frame) {
-        send(new DataFrame(new PongFrameType(), frame.getBytes()));
-        for (WebSocketListener listener : listeners) {
-            listener.onPing(this, frame.getBytes());
-        }
-    }
+  protected byte[] toRawData(byte[] binary) {
+    return toRawData(binary, true);
+  }
 
-    @Override
-    public void onPong(DataFrame frame) {
-        for (WebSocketListener listener : listeners) {
-            listener.onPong(this, frame.getBytes());
-        }
-    }
+  protected byte[] toRawData(String fragment, boolean last) {
+    final DataFrame dataFrame = protocolHandler.toDataFrame(fragment, last);
+    return protocolHandler.frame(dataFrame);
+  }
 
-    @Override
-    public void close() {
-        close(NORMAL_CLOSURE, null);
-    }
+  protected byte[] toRawData(byte[] binary, boolean last) {
+    final DataFrame dataFrame = protocolHandler.toDataFrame(binary, last);
+    return protocolHandler.frame(dataFrame);
+  }
 
-    @Override
-    public void close(int code) {
-        close(code, null);
-    }
+  @SuppressWarnings("unchecked")
+  protected void sendRaw(byte[] rawData) {
+    final Connection connection = protocolHandler.getConnection();
+    final MemoryManager mm = connection.getTransport().getMemoryManager();
+    final Buffer buffer = Buffers.wrap(mm, rawData);
+    buffer.allowBufferDispose(false);
 
-    @Override
-    public void close(int code, String reason) {
-        if (state.compareAndSet(State.CONNECTED, State.CLOSING)) {
-            protocolHandler.close(code, reason);
-        }
-    }
+    connection.write(buffer);
+  }
 
-    @Override
-    public GrizzlyFuture<DataFrame> send(byte[] data) {
-        if (isConnected()) {
-            return protocolHandler.send(data);
-        } else {
-            throw new RuntimeException("Socket is not connected.");
-        }
-    }
+  protected Broadcaster getBroadcaster() {
+    return broadcaster;
+  }
 
-    @Override
-    public GrizzlyFuture<DataFrame> send(String data) {
-        if (isConnected()) {
-            return protocolHandler.send(data);
-        } else {
-            throw new RuntimeException("Socket is not connected.");
-        }
-    }
-
-    @Override
-    public void broadcast(Iterable<? extends WebSocket> recipients,
-                          String data) {
-        if (state.get() == State.CONNECTED) {
-            broadcaster.broadcast(recipients, data);
-        } else {
-            throw new RuntimeException("Socket is already closed.");
-        }
-    }
-
-    @Override
-    public void broadcast(Iterable<? extends WebSocket> recipients,
-                          byte[] data) {
-        if (state.get() == State.CONNECTED) {
-            broadcaster.broadcast(recipients, data);
-        } else {
-            throw new RuntimeException("Socket is already closed.");
-        }
-    }
-
-    @Override
-    public void broadcastFragment(Iterable<? extends WebSocket> recipients,
-                                  String data, boolean last) {
-        if (state.get() == State.CONNECTED) {
-            broadcaster.broadcastFragment(recipients, data, last);
-        } else {
-            throw new RuntimeException("Socket is already closed.");
-        }
-    }
-
-    @Override
-    public void broadcastFragment(Iterable<? extends WebSocket> recipients,
-                                  byte[] data, boolean last) {
-        if (state.get() == State.CONNECTED) {
-            broadcaster.broadcastFragment(recipients, data, last);
-        } else {
-            throw new RuntimeException("Socket is already closed.");
-        }
-    }
-
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public GrizzlyFuture<DataFrame> sendPing(byte[] data) {
-        return send(new DataFrame(new PingFrameType(), data));
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public GrizzlyFuture<DataFrame> sendPong(byte[] data) {
-        return send(new DataFrame(new PongFrameType(), data));
-    }
-
-    private GrizzlyFuture<DataFrame> send(DataFrame frame) {
-        if (isConnected()) {
-            return protocolHandler.send(frame);
-        } else {
-            throw new RuntimeException("Socket is not connected.");
-        }
-    }
-
-    @Override
-    public GrizzlyFuture<DataFrame> stream(boolean last, String fragment) {
-        if (isConnected()) {
-            return protocolHandler.stream(last, fragment);
-        } else {
-            throw new RuntimeException("Socket is not connected.");
-        }
-    }
-
-    @Override
-    public GrizzlyFuture<DataFrame> stream(boolean last, byte[] bytes, int off, int len) {
-        if (isConnected()) {
-            return protocolHandler.stream(last, bytes, off, len);
-        } else {
-            throw new RuntimeException("Socket is not connected.");
-        }
-    }
-
-    protected byte[] toRawData(String text) {
-        return toRawData(text, true);
-    }
-
-    protected byte[] toRawData(byte[] binary) {
-        return toRawData(binary, true);
-    }
-
-    protected byte[] toRawData(String fragment, boolean last) {
-        final DataFrame dataFrame = protocolHandler.toDataFrame(fragment, last);
-        return protocolHandler.frame(dataFrame);
-    }
-
-    protected byte[] toRawData(byte[] binary, boolean last) {
-        final DataFrame dataFrame = protocolHandler.toDataFrame(binary, last);
-        return protocolHandler.frame(dataFrame);
-    }
-
-    @SuppressWarnings("unchecked")
-    protected void sendRaw(byte[] rawData) {
-        final Connection connection = protocolHandler.getConnection();
-        final MemoryManager mm = connection.getTransport().getMemoryManager();
-        final Buffer buffer = Buffers.wrap(mm, rawData);
-        buffer.allowBufferDispose(false);
-
-        connection.write(buffer);
-    }
-
-    protected Broadcaster getBroadcaster() {
-        return broadcaster;
-    }
-
-    protected void setBroadcaster(Broadcaster broadcaster) {
-        this.broadcaster = broadcaster;
-    }
+  protected void setBroadcaster(Broadcaster broadcaster) {
+    this.broadcaster = broadcaster;
+  }
 }
--- modules/websockets/src/main/java/org/glassfish/grizzly/websockets/Utils.java
+++ modules/websockets/src/main/java/org/glassfish/grizzly/websockets/Utils.java
@@ -39,141 +39,153 @@
  */
 package org.glassfish.grizzly.websockets;
 
+import static java.util.concurrent.CompletableFuture.completedFuture;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.CompletableFuture;
+
 import javax.servlet.ReadListener;
 import javax.servlet.ServletInputStream;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.WriteListener;
 
+import org.glassfish.grizzly.websockets.frametypes.BinaryFrameType;
+
 public final class Utils {
 
-    static final ServletInputStream NULL_SERVLET_INPUT_STREAM =
-            new ServletInputStream() {
+  static final ServletInputStream NULL_SERVLET_INPUT_STREAM =
+      new ServletInputStream() {
 
         @Override
         public int read() throws IOException {
-            return -1;
+          return -1;
         }
 
         @Override
         public boolean isFinished() {
-            return true;
+          return true;
         }
 
         @Override
         public boolean isReady() {
-            return true;
+          return true;
         }
 
         @Override
         public void setReadListener(final ReadListener readListener) {
-            try {
-                readListener.onAllDataRead();
-            } catch (IOException e) {
-                readListener.onError(e);
-            }
+          try {
+            readListener.onAllDataRead();
+          } catch (IOException e) {
+            readListener.onError(e);
+          }
         }
-    };
-    
-    static final Reader NULL_READER = new Reader() {
+      };
 
-        @Override
-        public int read(char[] cbuf, int off, int len) throws IOException {
-            return -1;
-        }
+  static final Reader NULL_READER = new Reader() {
 
-        @Override
-        public void close() throws IOException {
-        }
-
-    };
-    
-    static final ServletOutputStream NULL_SERVLET_OUTPUT_STREAM = new ServletOutputStream() {
-        private IOException ioe1;
-        private IOException ioe2;
-                
-        @Override
-        public boolean isReady() {
-            return true;
-        }
-
-        @Override
-        public void setWriteListener(final WriteListener writeListener) {
-            // we don't care if ioe1 will be initialized several times because of thread racing
-            if (ioe1 == null) {
-                ioe1 = new IOException("Can't write to a websocket using ServletOutputStream");
-            }
-            
-            writeListener.onError(ioe1);
-        }
-
-        @Override
-        public void write(final int b) throws IOException {
-            // we don't care if ioe2 will be initialized several times because of thread racing
-            if (ioe2 == null) {
-                ioe2 = new IOException("Can't write to a websocket using ServletOutputStream");
-            }
-            
-            throw ioe2;
-        }
-    };
-    
-    static final Writer NULL_WRITER = new Writer() {
-        private IOException ioe;
-
-        @Override
-        public void write(char[] cbuf, int off, int len) throws IOException {
-            // we don't care if ioe will be initialized several times because of thread racing
-            if (ioe == null) {
-                ioe = new IOException("Can't write to a websocket using ServletWriter");
-            }
-            
-            throw ioe;
-        }
-
-        @Override
-        public void flush() throws IOException {
-        }
-
-        @Override
-        public void close() throws IOException {
-        }
-        
-    };
-    
-    public static byte[] toArray(long length) {
-        long value = length;
-        byte[] b = new byte[8];
-        for (int i = 7; i >= 0 && value > 0; i--) {
-            b[i] = (byte) (value & 0xFF);
-            value >>= 8;
-        }
-        return b;
+    @Override
+    public int read(char[] cbuf, int off, int len) throws IOException {
+      return -1;
     }
 
-    public static long toLong(byte[] bytes, int start, int end) {
-        long value = 0;
-        for (int i = start; i < end; i++) {
-            value <<= 8;
-            value ^= (long) bytes[i] & 0xFF;
-        }
-        return value;
+    @Override
+    public void close() throws IOException {
     }
 
-    public static List<String> toString(byte[] bytes) {
-        return toString(bytes, 0, bytes.length);
+  };
+
+  static final ServletOutputStream NULL_SERVLET_OUTPUT_STREAM = new ServletOutputStream() {
+
+    private IOException ioe1;
+    private IOException ioe2;
+
+    @Override
+    public boolean isReady() {
+      return true;
     }
 
-    public static List<String> toString(byte[] bytes, int start, int end) {
-        List<String> list = new ArrayList<String>();
-        for (int i = start; i < end; i++) {
-            list.add(Integer.toHexString(bytes[i] & 0xFF).toUpperCase(Locale.US));
-        }
-        return list;
+    @Override
+    public void setWriteListener(final WriteListener writeListener) {
+      // we don't care if ioe1 will be initialized several times because of thread racing
+      if (ioe1 == null) {
+        ioe1 = new IOException("Can't write to a websocket using ServletOutputStream");
+      }
+
+      writeListener.onError(ioe1);
     }
+
+    @Override
+    public void write(final int b) throws IOException {
+      // we don't care if ioe2 will be initialized several times because of thread racing
+      if (ioe2 == null) {
+        ioe2 = new IOException("Can't write to a websocket using ServletOutputStream");
+      }
+
+      throw ioe2;
+    }
+  };
+
+  static final Writer NULL_WRITER = new Writer() {
+
+    private IOException ioe;
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+      // we don't care if ioe will be initialized several times because of thread racing
+      if (ioe == null) {
+        ioe = new IOException("Can't write to a websocket using ServletWriter");
+      }
+
+      throw ioe;
+    }
+
+    @Override
+    public void flush() throws IOException {
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+
+  };
+
+  public static byte[] toArray(long length) {
+    long value = length;
+    byte[] b = new byte[8];
+    for (int i = 7; i >= 0 && value > 0; i--) {
+      b[i] = (byte) (value & 0xFF);
+      value >>= 8;
+    }
+    return b;
+  }
+
+  public static long toLong(byte[] bytes, int start, int end) {
+    long value = 0;
+    for (int i = start; i < end; i++) {
+      value <<= 8;
+      value ^= (long) bytes[i] & 0xFF;
+    }
+    return value;
+  }
+
+  public static List<String> toString(byte[] bytes) {
+    return toString(bytes, 0, bytes.length);
+  }
+
+  public static List<String> toString(byte[] bytes, int start, int end) {
+    List<String> list = new ArrayList<>();
+    for (int i = start; i < end; i++) {
+      list.add(Integer.toHexString(bytes[i] & 0xFF).toUpperCase(Locale.US));
+    }
+    return list;
+  }
+
+  public static CompletableFuture<DataFrame> completedFrame(byte[] fragment, boolean last) {
+    return completedFuture(new DataFrame(new BinaryFrameType(), fragment, last));
+  }
 }
