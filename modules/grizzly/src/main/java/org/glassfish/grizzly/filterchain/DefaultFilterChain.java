fb734f2d0ee71959559ff791c6c08858bd6675ea
Ezequiel Werner <eze210@gmail.com>
2020-10-23 15:27:34 -0300

    Add referennce

--- modules/grizzly/src/main/java/org/glassfish/grizzly/filterchain/DefaultFilterChain.java
+++ modules/grizzly/src/main/java/org/glassfish/grizzly/filterchain/DefaultFilterChain.java
@@ -509,6 +509,10 @@ public final class DefaultFilterChain extends ListFacadeFilterChain {
 
         if (filtersState != null) {
             Object message = ctx.getMessage();
+
+            // This change was introduced in:
+            // PAYARA-3288 memory leak in DefaultFilterChain, resulting in large amounts of HeapBuffer
+            // https://github.com/eclipse-ee4j/grizzly/commit/c435680a1da09c15682b21370737e49df825bf1f
             if (message instanceof Buffer) {
                 Buffer bufferMessage = (Buffer) message;
                 if (!bufferMessage.hasRemaining()) {
